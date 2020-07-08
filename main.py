9573dce8ed79a015d312b5d939f9036c7907ef48
Dennis Wentzien <wentzien@users.noreply.github.com>
2020-07-08 17:18:34 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import time
+import playsound
+import speech_recognition as sr
+from gtts import gTTS
+
+def speak(text):
+    tts = gTTS(text=text, lang='en')
+    filename = 'voice.mp3'
+    tts.save(filename)
+    playsound.playsound(filename)
+
+
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        said = ''
+
+        try:
+            said = r.recognize_google(audio)
+            print(said)
+        except Exception as e:
+            print('Exception:' + str(e))
+    return said
+
+
+# speak('good evening guys')
+said = get_audio()
+
+if 'hello' in said:
+    speak('Hello, how are you?')
